4d6b288d01b7ec7d6bfbc81e4c4976fc91bd33f0
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-23 13:03:39 +0200

    a

--- include/ph_data_structures/linked_list.hpp
+++ include/ph_data_structures/linked_list.hpp
@@ -1,1 +1,108 @@
+/**
+ - access to elements: O (n)
+ - insertion: O (n)
+ */
+
+
+
 #pragma once
+#define fwd(x) std::forward <decltype (x)> (x)
+
+namespace ph::data_structures
+{
+    enum struct state
+    {
+        
+    };
+    
+    template <typename T>
+    struct node
+    {
+        constexpr node () noexcept : _next {nullptr}
+        {
+            
+        }
+        constexpr node (auto&& value) noexcept : _value {fwd (value)}, _next {nullptr}
+        {
+            
+        }
+        constexpr node (auto&& first, auto&&... rest) noexcept : _value {fwd (first)}, _next {new node <T> {fwd (rest)...}}
+        {
+            
+        }
+        constexpr ~node () noexcept
+        {
+            if (_next)
+            {
+                delete _next;
+            }
+        }
+        
+        
+    private:
+        T _value;
+        node <T> * _next {nullptr};
+    };
+    
+    template <typename T>
+    struct linked_list
+    {
+        constexpr linked_list () noexcept = default;
+        constexpr linked_list (auto&& first, auto&&... rest) noexcept : _root {new node <T> {fwd (first), fwd (rest)...}}, _size (sizeof... (rest) + 1)
+        {
+            
+        }
+        constexpr auto size () const noexcept -> size_t
+        {
+            return _size;
+        }
+        constexpr ~linked_list () noexcept
+        {
+            if (_root)
+            {
+                delete _root;
+            }
+        }
+        // access to element O (n)
+        constexpr auto operator[] (size_t k) noexcept -> T&
+        {
+            auto* curr = _root;
+            
+            for (int i = 0; i < k; ++i)
+            {
+                curr = curr -> next;
+            }
+            
+            return curr -> _value;
+        }
+        // access to element O (n)
+        constexpr auto operator[] (size_t k) const noexcept -> T
+        {
+            auto* curr = _root;
+            
+            for (int i = 0; i < k; ++i)
+            {
+                curr = curr -> next;
+            }
+            
+            return curr -> _value;
+        }
+        // insertion O (n)
+        constexpr auto insert (size_t k, auto&& value) noexcept -> void
+        {
+            auto * curr = _root;
+            for (int i = 0; i < k; ++i)
+            {
+                curr = curr -> _next;
+            }
+            
+//            curr ->
+        }
+    private:
+        node <T> * _root {nullptr};
+        size_t _size {0};
+    };
+}
+
+
+#undef fwd
--- tests/test_data_structures.cpp
+++ tests/test_data_structures.cpp
@@ -1,13 +1,114 @@
 #include <catch2/catch.hpp>
 #include <ph_data_structures/data_structures.hpp>
+#include <ph_concepts/concepts.hpp>
 
 
+//namespace std{
+//auto begin (auto&& c) {
+//    return c;
+//}
+//}
+//auto begin (auto&& c) {
+//    return c;
+//}
 
 namespace ph::data_structures
 {
     TEST_CASE ("stack")
     {
         auto s_0 = stack {1, 2, 3};
-        REQUIRE (s_0.top() == 3);
+    }
+    
+    TEST_CASE ("linked_list")
+    {
+        auto s_0 = linked_list <int> {1, 2, 3};
+    }
+}
+    
+namespace ph::virtual_machine
+{
+    using std::cout, std::endl;
+    using namespace ph::concepts;
+    
+    
+    
+    
+    auto lex = [] (String auto && source)
+    {
+        enum
+        {
+            BEGIN,
+            NUMBER,
+            WHITESPACE
+        };
+        
+        auto current = BEGIN;
+        
+        // loop through source string
+        for (auto && c : source)
+        {
+            cout << c << endl;
+            
+            // check current state
+            switch (current)
+            {
+                case BEGIN:
+                {
+                    if (isdigit (c))
+                    {
+                        // change current state
+                        current = NUMBER;
+                    }
+                    
+                    break;
+                }
+                    
+                case NUMBER:
+                {
+                    if (not isdigit (c))
+                    {
+                        if (c == ' ' or c == '\n' or c == '\t')
+                        {
+                            
+                            
+                        } else if (c == '.')
+                        {
+                            
+                        }
+                    }
+                    break;
+                }
+                    
+                default:
+                {
+                    break;
+                }
+            }
+            
+        }
+    };
+    
+    
+    enum
+    {
+        PUSH
+    };
+    
+    auto vm = [] (Range auto && stack, Range auto && code)
+    {
+        for (auto && instruction : code)
+        {
+            
+        }
+    };
+    
+    TEST_CASE ("")
+    {
+        
+        lex ("1 + 2 * 3 + 4");
+//        1 + 2 * 3 + 4 = 11
+        int stack [] = {1, 2, 3, 4};
+        
+        
     }
 }
