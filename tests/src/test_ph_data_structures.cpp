00c1390b34061544500fc9a8094e2988c0f9cb7d
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-22 11:28:48 +0200

    a

---
+++ tests/src/test_ph_data_structures.cpp
@@ -0,0 +1,27 @@
+#include <catch2/catch.hpp>
+//#include <ph_concepts/concepts.hpp>
+#include <ph_data_structures/data_structures.hpp>
+
+using std::cout, std::endl;
+
+TEST_CASE ("main")
+{
+    
+    cout << "hi" << endl;
+}
+
+
+TEST_CASE ("read file")
+{
+    SECTION ("char const*")
+    {
+        // char const* path {__FILE__};
+        // ph::file::read (path);
+        
+    }
+    
+    SECTION ("std::string")
+    {
+        
+    }
+}
